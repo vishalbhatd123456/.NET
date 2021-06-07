93b1f6866717c58199e0033c64585a4da5843ae2
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2021-06-07 11:47:17 +0530

    Create DisplayHTMLOfGoogle.cs

---
+++ DisplayHTMLOfGoogle.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+
+namespace SampleReq_1
+{
+    class Program
+    {
+	static void Main(string[] args)
+	{
+	    //initiate the get request
+	    GetRequest("http://www.google.com.pk");
+
+
+	    Console.ReadKey();
+	}
+
+	async static void GetRequest(string url)
+	{
+	    using (HttpClient client = new HttpClient())
+	    {
+		//create a client, then get a response from the server, discard the client once the  the connection is no longer needed.
+		//async to the server
+		using(HttpResponseMessage response = await client.GetAsync(url))
+		{
+		  using (HttpContent content = response.Content)
+		    {
+			string myContent = await content.ReadAsStringAsync();
+			Console.WriteLine(myContent);
+		    }
+		}
+
+	    }
+	}
+    }
+}
